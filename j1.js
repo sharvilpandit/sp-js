952f9393f5d4c24c4b91e2d632458e92189b393e
keyurmeandev <keyur.rathod84@gmail.com>
2022-07-20 19:03:32 +0530

    basic practise

---
+++ j1.js
@@ -0,0 +1,29 @@
+console.log("Hello Pc");
+
+var myname="sharvil";
+var lastname="pandit";
+
+console.log("Hello, " +myname + " "  +lastname);
+
+const message="Hello world";
+//alert(message);
+
+let heroes=["sh"];
+let heroines=[];
+console.log(heroes);
+heroes[1]="ak";
+heroines[0]="ab";
+heroes=heroes.concat(heroines);
+console.log(heroes);
+
+
+
+    let hey=["ss","sp","kr","up"];
+    console.log(hey);
+    hey.pop();
+    hey.push("iz");
+    hey.shift();
+    hey.unshift("am");
+    hey.sort();
+    console.log(hey);
+
