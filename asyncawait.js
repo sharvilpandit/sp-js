f2ca0eea9fc457d6652d27c1150c2ad6a99fbdac
keyurmeandev <keyur.rathod84@gmail.com>
2022-08-05 19:06:59 +0530

    Avanced

--- asyncawait.js
+++ asyncawait.js
@@ -1,25 +1,6 @@
-async function showAvatar() {
 
-    // read our JSON
-    let response = await fetch('/article/promise-chaining/user.json');
-    let user = await response.json();
-  
-    // read github user
-    let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
-    let githubUser = await githubResponse.json();
-  
-    // show the avatar
-    let img = document.createElement('img');
-    img.src = githubUser.avatar_url;
-    img.className = "promise-avatar-example";
-    document.body.append(img);
-  
-    // wait 3 seconds
-    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-  
-    img.remove();
-  
-    return githubUser;
-  }
-  
-  showAvatar();
+  Promise.all([
+    new Promise(resolve => setTimeout(() => resolve(1), 3000)), // 1
+    new Promise(resolve => setTimeout(() => resolve(2), 2000)), // 2
+    new Promise(resolve => setTimeout(() => resolve(3), 1000))  // 3
+  ]).then(alert).then(console.log("wait"));
